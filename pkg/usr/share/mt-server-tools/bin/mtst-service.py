4b08b208ce73b9820d7aef235d0f5c021797ff33
Tai Kedzierski <dch tai gmail>
2017-08-29 23:15:10 +0100

    preliminary parse args

--- pkg/usr/share/mt-server-tools/bin/mtst-service.py
+++ pkg/usr/share/mt-server-tools/bin/mtst-service.py
@@ -1,3 +1,36 @@
 #!/usr/bin/python3
 
+from jinja2 import Template
+import re
+import argparse
 
+service_template_location = "/usr/share/mt-server-tools/resources/service.template"
+service_destination = "/etc/systemd/system/%s.service"
+
+args = argparse.ArgumentParser()
+args.add_argument("")
+
+'''
+Required:
+    user_name
+    command_string
+
+Optional:
+    description
+    after
+    restart
+    wanted_by
+'''
+
+def openTemplate(path):
+    fh = open(path)
+    lines = fh.readlines()
+    fh.close()
+    
+    return Template( "".join(lines) )
+
+def varify(name):
+    return re.replace(name, "[^a-zA-Z0-9]+", "_")
+
+def getServiceDestination(name):
+    return service_destination % name
