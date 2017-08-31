cecaf549769f33d43f9af7641b6b770b03a481ba
Tai Kedzierski <dch tai gmail>
2017-08-31 23:15:10 +0100

    add location for accounts

--- sh-src/default-locations.sh
+++ sh-src/default-locations.sh
@@ -2,3 +2,4 @@ MTST_bins=/usr/share/mt-server-tools/bin
 MTST_service_template=/usr/share/mt-server-tools/resources/service.template
 MTST_default_config=/usr/share/mt-server-tools/resources/mintest.conf.template
 MTST_default_worlds=/usr/share/mt-server-tools/resources/default-worlds
+MTST_accounts=/etc/mt-server-tools/accounts.txt
