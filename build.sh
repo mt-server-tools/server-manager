5d4836892af2e9d8de20666312823723044b1785
Tai Kedzierski <dch tai gmail>
2017-09-06 16:38:03 +0100

    add scripts to build routine

--- build.sh
+++ build.sh
@@ -1,2 +1,11 @@
+# This is a developer script.
+# Before comitting a release, run this script
+# You will need bash-builder [1] to run the build task
+#
+# [1] https://github.com/taikedz/bash-builder
+
+# Build the main install script
 BUILDOUTD=./ bbuild sh-src/install
 
+# Build the main executable
+BUILDOUTD=./pkg/usr/share/mt-server-tools/bin bbuild sh-src/mtst
