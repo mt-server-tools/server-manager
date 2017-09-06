8100ea7cd903b5feeb4c3f6eb4723bc82843c4cf
Tai Kedzierski <dch tai gmail>
2017-09-06 12:49:19 +0100

    additional general tests

--- tests/new-server-test.sh
+++ tests/new-server-test.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
-cd "$(dirname "$0")"
+cd "$(dirname "$0")/.."
 
-. test_framework.sh
+. tests/test_framework.sh
 
-. ../sh-src/new-server.sh
+. sh-src/new-server.sh
+
+t:report "Test parse_args"
 
 t:expect "parse_args should error on fail" "$(newserver:parse_args)" '^\s*$'
 
@@ -14,3 +16,29 @@ t:expect "Hostname" "$MTST_hostname" "example.com"
 t:expect "Admin" "$MTST_game_admin" "myadmin"
 t:expect "Title" "$MTST_game_title" "A title"
 t:expect "Server name" "$MTST_sys_servername" "servername"
+
+t:report "Test accounts/ports data"
+
+MTST_accounts="./accounts.tmp"
+
+# FIXME faulty test
+t:expect "Failure on access non-existent account" "$(newserver:get_available_port 2>&1)" "^\S+$"
+
+echo "# account:port" > "$MTST_accounts"
+MTST_sys_servername="server1"
+
+newserver:get_available_port
+t:expect "Get initial port number" "$MTST_port" "30000"
+newserver:register_port
+newserver:get_available_port
+t:expect "Get next port number" "$MTST_port" "30001"
+
+# FIXME set the config and service file locations
+
+t:report "Check config data"
+newserver:get_config_data
+
+t:report "Check service data"
+newserver:get_service_data
+
+echo "$MTST_config_data"
